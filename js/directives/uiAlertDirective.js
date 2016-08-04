5fd37f11fec36a82f58e924fe2dfd28a807b99d1
gpzim98 <gpzim98@gmail.com>
2016-08-04 19:43:40 +0100

    Using replace set the template without the div outside and restrict to decide if the directiv will be used like an attribute, element, class or both

--- js/directives/uiAlertDirective.js
+++ js/directives/uiAlertDirective.js
@@ -1,5 +1,10 @@
 angular.module('listaTelefonica').directive('uiAlert', function(){
   return {
-    templateUrl: 'view/alert.html'
+    templateUrl: 'view/alert.html',
+    replace: true,
+    // restrict: 'A'
+    restrict: 'E',
+    // restrict: 'C'
+    // restrict: 'AE'
   };
 });
